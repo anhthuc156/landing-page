05aa9adbdc19021cc979f7f4648d6c78665bf0c2
AnhThuc <thuclva@twedu.com.vn>
2020-09-14 13:44:41 +0700

    Updates

---
+++ precache-manifest.3efa59713bb623ac06fd93dd76b05129.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c43d273e5e54f427d57911da59190f2",
+    "url": "/landingpage.github.io/index.html"
+  },
+  {
+    "revision": "b0589cd9ac990e152f11",
+    "url": "/landingpage.github.io/static/css/main.a9fa0003.chunk.css"
+  },
+  {
+    "revision": "d0b47aa6e094fcb5301c",
+    "url": "/landingpage.github.io/static/js/2.abd3cf6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landingpage.github.io/static/js/2.abd3cf6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0589cd9ac990e152f11",
+    "url": "/landingpage.github.io/static/js/main.8c334e13.chunk.js"
+  },
+  {
+    "revision": "6e491c63296c76d59c79",
+    "url": "/landingpage.github.io/static/js/runtime-main.0a8176e0.js"
+  },
+  {
+    "revision": "503c0269e79655aadb7785c5f495e8bc",
+    "url": "/landingpage.github.io/static/media/model-3-hero-desktop-v2-1.503c0269.jpg"
+  }
+]);
